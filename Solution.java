0c72327a4684b41ca91c59d0329fada219f68f85
Kshitij Kotecha <kotechakshitij@gmail.com>
2024-11-11 14:30:52 +0530

    Solution.java

--- Solution.java
+++ Solution.java
@@ -192,7 +192,7 @@ class ExpenseTracker {
 }
 
 // Main class for user interaction
-public class Main {
+public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
